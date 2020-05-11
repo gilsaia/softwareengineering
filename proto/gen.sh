7d8110451294b3bf7056a24b8ea871216d1b5ad2
gilsaia <794433219@qq.com>
2020-05-12 01:16:41 +0800

    simple_test

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,2 +1,3 @@
 protoc --go_out=../src/ simple_test.proto
 protoc --go_out=plugins=grpc:../src/ simple_test.proto
+protoc -I. -I=$GOPATH/src -I=../pkg/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:../src/ simple_test.proto
