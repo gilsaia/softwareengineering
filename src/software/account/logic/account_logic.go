af8135889ece529cdac9ab32c80eaa286bcca61b
gilsaia <794433219@qq.com>
2020-05-30 10:19:43 +0800

    user fix

--- src/software/account/logic/account_logic.go
+++ src/software/account/logic/account_logic.go
@@ -47,6 +47,7 @@ func (logic AccountLogic) Verify(cellphone string) common.BgErr {
 	}
 	err = model.CreateUser(db, user)
 	if err != nil {
+		db, _ = model.NewDbConnection()
 		err = model.UpdateUser(db, cellphone, user)
 		return common.CustomErr(common.DbErr, err)
 	}
